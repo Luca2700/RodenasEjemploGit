25dc7a80cf4091710f46ee6f88320c7965b3b512
Lucar <Lucar@LAPTOP-QRNHBNUE>
2022-05-12 17:14:30 +0200

    Version1

---
+++ src/rodenasejemplogit/RodenasEjemploGit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rodenasejemplogit;
+
+/**
+ *
+ * @author Lucar
+ */
+public class RodenasEjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Luca");
+        System.out.println("Version 1.0");
+    }
+    
+}
